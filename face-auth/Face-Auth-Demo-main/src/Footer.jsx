e44e98c59a17af037776897c93a9a9de4fe7fce0
khadarfhareedm <160720923+khadarfhareedm@users.noreply.github.com>
2025-05-08 12:59:10 +0530

    Add files via upload

---
+++ face-auth/Face-Auth-Demo-main/src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto text-center">
+       
+      
+        
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
